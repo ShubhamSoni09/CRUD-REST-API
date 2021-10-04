3635e91bf9502a96ec94050f8a586ebb866dcb57
Shubham Soni <18bec110@nirmauni.ac.in>
2021-10-04 17:01:55 +0530

    routing completed

--- CRUDJS/app.js
+++ CRUDJS/app.js
@@ -1,4 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const url = 'SoniDB'
 const app = express();
+
+
+mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open',function(){
+    console.log('Connected...')
+})
